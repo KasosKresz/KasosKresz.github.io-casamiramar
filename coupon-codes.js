ee5f54b2b4aa30b4182b6db75e4d01b8f2a7a6ae
KasosKresz <107881355+KasosKresz@users.noreply.github.com>
2026-04-25 19:42:01 +0200

    Add files via upload

---
+++ coupon-codes.js
@@ -0,0 +1,24 @@
+window.SierraMarCoupons = {
+  codes: {
+    WELCOMEA20: { discountPercent: 20 },
+    WELCOMEB20: { discountPercent: 20 },
+    WELCOMEC20: { discountPercent: 20 },
+    THANKYOUA20: { discountPercent: 20 },
+    THANKYOUB20: { discountPercent: 20 },
+    THANKYOUC20: { discountPercent: 20 },
+    FRIENDSA20: { discountPercent: 20 },
+    FRIENDSB20: { discountPercent: 20 },
+    FRIENDSC20: { discountPercent: 20 },
+    RETURNA20: { discountPercent: 20 },
+    RETURNB20: { discountPercent: 20 },
+    RETURNC20: { discountPercent: 20 },
+    BOOKNOWA20: { discountPercent: 20 },
+    BOOKNOWB20: { discountPercent: 20 },
+    BOOKNOWC20: { discountPercent: 20 },
+    GETAWAYA20: { discountPercent: 20 },
+    GETAWAYB20: { discountPercent: 20 },
+    GETAWAYC20: { discountPercent: 20 },
+    GUESTA20: { discountPercent: 20 },
+    GUESTB20: { discountPercent: 20 }
+  }
+};
