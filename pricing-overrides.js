26df8fd959fadd16f9010e8f6a5589fdfe02f946
KasosKresz <107881355+KasosKresz@users.noreply.github.com>
2026-04-28 00:18:32 +0200

    Add files via upload

---
+++ pricing-overrides.js
@@ -0,0 +1,751 @@
+window.SierraMarPricingOverrides = {
+  "dates": {
+    "2026-05-07": {
+      "nightlyRate": 50
+    },
+    "2026-05-08": {
+      "nightlyRate": 50
+    },
+    "2026-05-09": {
+      "nightlyRate": 50
+    },
+    "2026-05-10": {
+      "nightlyRate": 50
+    },
+    "2026-05-11": {
+      "nightlyRate": 50
+    },
+    "2026-05-25": {
+      "nightlyRate": 50
+    },
+    "2026-05-26": {
+      "nightlyRate": 50
+    },
+    "2026-05-27": {
+      "nightlyRate": 50
+    },
+    "2026-05-31": {
+      "nightlyRate": 70
+    },
+    "2026-06-01": {
+      "nightlyRate": 70
+    },
+    "2026-06-02": {
+      "nightlyRate": 70
+    },
+    "2026-06-03": {
+      "nightlyRate": 70
+    },
+    "2026-06-04": {
+      "nightlyRate": 70
+    },
+    "2026-06-07": {
+      "nightlyRate": 80
+    },
+    "2026-06-08": {
+      "nightlyRate": 80
+    },
+    "2026-06-09": {
+      "nightlyRate": 80
+    },
+    "2026-06-10": {
+      "nightlyRate": 80
+    },
+    "2026-06-11": {
+      "nightlyRate": 80
+    },
+    "2026-06-12": {
+      "nightlyRate": 100
+    },
+    "2026-06-13": {
+      "nightlyRate": 100
+    },
+    "2026-06-14": {
+      "nightlyRate": 80
+    },
+    "2026-06-15": {
+      "nightlyRate": 80
+    },
+    "2026-06-16": {
+      "nightlyRate": 80
+    },
+    "2026-06-17": {
+      "nightlyRate": 80
+    },
+    "2026-06-18": {
+      "nightlyRate": 80
+    },
+    "2026-06-19": {
+      "nightlyRate": 100
+    },
+    "2026-06-20": {
+      "nightlyRate": 100
+    },
+    "2026-06-21": {
+      "nightlyRate": 100
+    },
+    "2026-06-22": {
+      "nightlyRate": 100
+    },
+    "2026-06-23": {
+      "nightlyRate": 100
+    },
+    "2026-06-24": {
+      "nightlyRate": 100
+    },
+    "2026-06-25": {
+      "nightlyRate": 100
+    },
+    "2026-06-26": {
+      "nightlyRate": 100
+    },
+    "2026-06-27": {
+      "nightlyRate": 100
+    },
+    "2026-06-28": {
+      "nightlyRate": 100
+    },
+    "2026-06-29": {
+      "nightlyRate": 100
+    },
+    "2026-06-30": {
+      "nightlyRate": 100
+    },
+    "2026-09-01": {
+      "nightlyRate": 80
+    },
+    "2026-09-02": {
+      "nightlyRate": 80
+    },
+    "2026-09-03": {
+      "nightlyRate": 80
+    },
+    "2026-09-04": {
+      "nightlyRate": 80
+    },
+    "2026-09-05": {
+      "nightlyRate": 80
+    },
+    "2026-09-06": {
+      "nightlyRate": 80
+    },
+    "2026-09-07": {
+      "nightlyRate": 80
+    },
+    "2026-09-08": {
+      "nightlyRate": 80
+    },
+    "2026-09-09": {
+      "nightlyRate": 80
+    },
+    "2026-09-10": {
+      "nightlyRate": 80
+    },
+    "2026-09-11": {
+      "nightlyRate": 80
+    },
+    "2026-09-12": {
+      "nightlyRate": 80
+    },
+    "2026-09-13": {
+      "nightlyRate": 80
+    },
+    "2026-09-14": {
+      "nightlyRate": 80
+    },
+    "2026-09-15": {
+      "nightlyRate": 80
+    },
+    "2026-09-16": {
+      "nightlyRate": 80
+    },
+    "2026-09-17": {
+      "nightlyRate": 80
+    },
+    "2026-09-18": {
+      "nightlyRate": 80
+    },
+    "2026-09-19": {
+      "nightlyRate": 80
+    },
+    "2026-09-20": {
+      "nightlyRate": 80
+    },
+    "2026-09-21": {
+      "nightlyRate": 80
+    },
+    "2026-09-22": {
+      "nightlyRate": 80
+    },
+    "2026-09-23": {
+      "nightlyRate": 80
+    },
+    "2026-09-24": {
+      "nightlyRate": 80
+    },
+    "2026-09-25": {
+      "nightlyRate": 80
+    },
+    "2026-09-26": {
+      "nightlyRate": 80
+    },
+    "2026-09-27": {
+      "nightlyRate": 80
+    },
+    "2026-09-28": {
+      "nightlyRate": 80
+    },
+    "2026-09-29": {
+      "nightlyRate": 80
+    },
+    "2026-09-30": {
+      "nightlyRate": 80
+    },
+    "2026-10-01": {
+      "nightlyRate": 70
+    },
+    "2026-10-02": {
+      "nightlyRate": 70
+    },
+    "2026-10-03": {
+      "nightlyRate": 70
+    },
+    "2026-10-04": {
+      "nightlyRate": 70
+    },
+    "2026-10-05": {
+      "nightlyRate": 70
+    },
+    "2026-10-06": {
+      "nightlyRate": 70
+    },
+    "2026-10-07": {
+      "nightlyRate": 70
+    },
+    "2026-10-08": {
+      "nightlyRate": 70
+    },
+    "2026-10-09": {
+      "nightlyRate": 70
+    },
+    "2026-10-10": {
+      "nightlyRate": 70
+    },
+    "2026-10-11": {
+      "nightlyRate": 70
+    },
+    "2026-10-12": {
+      "nightlyRate": 70
+    },
+    "2026-10-13": {
+      "nightlyRate": 70
+    },
+    "2026-10-14": {
+      "nightlyRate": 70
+    },
+    "2026-10-15": {
+      "nightlyRate": 70
+    },
+    "2026-10-16": {
+      "nightlyRate": 70
+    },
+    "2026-10-17": {
+      "nightlyRate": 70
+    },
+    "2026-10-18": {
+      "nightlyRate": 70
+    },
+    "2026-10-19": {
+      "nightlyRate": 70
+    },
+    "2026-10-20": {
+      "nightlyRate": 70
+    },
+    "2026-10-21": {
+      "nightlyRate": 70
+    },
+    "2026-10-22": {
+      "nightlyRate": 70
+    },
+    "2026-10-23": {
+      "nightlyRate": 70
+    },
+    "2026-10-24": {
+      "nightlyRate": 70
+    },
+    "2026-10-25": {
+      "nightlyRate": 70
+    },
+    "2026-10-26": {
+      "nightlyRate": 70
+    },
+    "2026-10-27": {
+      "nightlyRate": 70
+    },
+    "2026-10-28": {
+      "nightlyRate": 70
+    },
+    "2026-10-29": {
+      "nightlyRate": 70
+    },
+    "2026-10-30": {
+      "nightlyRate": 70
+    },
+    "2026-10-31": {
+      "nightlyRate": 70
+    },
+    "2026-11-01": {
+      "nightlyRate": 60
+    },
+    "2026-11-02": {
+      "nightlyRate": 60
+    },
+    "2026-11-03": {
+      "nightlyRate": 60
+    },
+    "2026-11-04": {
+      "nightlyRate": 60
+    },
+    "2026-11-05": {
+      "nightlyRate": 60
+    },
+    "2026-11-06": {
+      "nightlyRate": 60
+    },
+    "2026-11-07": {
+      "nightlyRate": 60
+    },
+    "2026-11-08": {
+      "nightlyRate": 60
+    },
+    "2026-11-09": {
+      "nightlyRate": 60
+    },
+    "2026-11-10": {
+      "nightlyRate": 60
+    },
+    "2026-11-11": {
+      "nightlyRate": 60
+    },
+    "2026-11-12": {
+      "nightlyRate": 60
+    },
+    "2026-11-13": {
+      "nightlyRate": 60
+    },
+    "2026-11-14": {
+      "nightlyRate": 60
+    },
+    "2026-11-15": {
+      "nightlyRate": 60
+    },
+    "2026-11-16": {
+      "nightlyRate": 60
+    },
+    "2026-11-17": {
+      "nightlyRate": 60
+    },
+    "2026-11-18": {
+      "nightlyRate": 60
+    },
+    "2026-11-19": {
+      "nightlyRate": 60
+    },
+    "2026-11-20": {
+      "nightlyRate": 60
+    },
+    "2026-11-21": {
+      "nightlyRate": 60
+    },
+    "2026-11-22": {
+      "nightlyRate": 60
+    },
+    "2026-11-23": {
+      "nightlyRate": 60
+    },
+    "2026-11-24": {
+      "nightlyRate": 60
+    },
+    "2026-11-25": {
+      "nightlyRate": 60
+    },
+    "2026-11-26": {
+      "nightlyRate": 60
+    },
+    "2026-11-27": {
+      "nightlyRate": 60
+    },
+    "2026-11-28": {
+      "nightlyRate": 60
+    },
+    "2026-11-29": {
+      "nightlyRate": 60
+    },
+    "2026-11-30": {
+      "nightlyRate": 60
+    },
+    "2026-12-01": {
+      "nightlyRate": 60
+    },
+    "2026-12-02": {
+      "nightlyRate": 60
+    },
+    "2026-12-03": {
+      "nightlyRate": 60
+    },
+    "2026-12-04": {
+      "nightlyRate": 60
+    },
+    "2026-12-05": {
+      "nightlyRate": 60
+    },
+    "2026-12-06": {
+      "nightlyRate": 60
+    },
+    "2026-12-07": {
+      "nightlyRate": 60
+    },
+    "2026-12-08": {
+      "nightlyRate": 60
+    },
+    "2026-12-09": {
+      "nightlyRate": 60
+    },
+    "2026-12-10": {
+      "nightlyRate": 60
+    },
+    "2026-12-11": {
+      "nightlyRate": 60
+    },
+    "2026-12-12": {
+      "nightlyRate": 60
+    },
+    "2026-12-13": {
+      "nightlyRate": 60
+    },
+    "2026-12-14": {
+      "nightlyRate": 80
+    },
+    "2026-12-15": {
+      "nightlyRate": 80
+    },
+    "2026-12-16": {
+      "nightlyRate": 80
+    },
+    "2026-12-17": {
+      "nightlyRate": 80
+    },
+    "2026-12-18": {
+      "nightlyRate": 80
+    },
+    "2026-12-19": {
+      "nightlyRate": 80
+    },
+    "2026-12-20": {
+      "nightlyRate": 80
+    },
+    "2026-12-21": {
+      "nightlyRate": 80
+    },
+    "2026-12-22": {
+      "nightlyRate": 80
+    },
+    "2026-12-23": {
+      "nightlyRate": 80
+    },
+    "2026-12-24": {
+      "nightlyRate": 80
+    },
+    "2026-12-25": {
+      "nightlyRate": 80
+    },
+    "2026-12-26": {
+      "nightlyRate": 80
+    },
+    "2026-12-27": {
+      "nightlyRate": 80
+    },
+    "2026-12-28": {
+      "nightlyRate": 80
+    },
+    "2026-12-29": {
+      "nightlyRate": 80
+    },
+    "2026-12-30": {
+      "nightlyRate": 80
+    },
+    "2026-12-31": {
+      "nightlyRate": 80
+    },
+    "2027-01-01": {
+      "nightlyRate": 80
+    },
+    "2027-01-02": {
+      "nightlyRate": 80
+    },
+    "2027-01-03": {
+      "nightlyRate": 80
+    },
+    "2027-01-04": {
+      "nightlyRate": 80
+    },
+    "2027-01-05": {
+      "nightlyRate": 80
+    },
+    "2027-01-06": {
+      "nightlyRate": 80
+    },
+    "2027-01-07": {
+      "nightlyRate": 40
+    },
+    "2027-01-08": {
+      "nightlyRate": 40
+    },
+    "2027-01-09": {
+      "nightlyRate": 40
+    },
+    "2027-01-10": {
+      "nightlyRate": 40
+    },
+    "2027-01-11": {
+      "nightlyRate": 40
+    },
+    "2027-01-12": {
+      "nightlyRate": 40
+    },
+    "2027-01-13": {
+      "nightlyRate": 40
+    },
+    "2027-01-14": {
+      "nightlyRate": 40
+    },
+    "2027-01-15": {
+      "nightlyRate": 40
+    },
+    "2027-01-16": {
+      "nightlyRate": 40
+    },
+    "2027-01-17": {
+      "nightlyRate": 40
+    },
+    "2027-01-18": {
+      "nightlyRate": 40
+    },
+    "2027-01-19": {
+      "nightlyRate": 40
+    },
+    "2027-01-20": {
+      "nightlyRate": 40
+    },
+    "2027-01-21": {
+      "nightlyRate": 40
+    },
+    "2027-01-22": {
+      "nightlyRate": 40
+    },
+    "2027-01-23": {
+      "nightlyRate": 40
+    },
+    "2027-01-24": {
+      "nightlyRate": 40
+    },
+    "2027-01-25": {
+      "nightlyRate": 40
+    },
+    "2027-01-26": {
+      "nightlyRate": 40
+    },
+    "2027-01-27": {
+      "nightlyRate": 40
+    },
+    "2027-01-28": {
+      "nightlyRate": 40
+    },
+    "2027-01-29": {
+      "nightlyRate": 40
+    },
+    "2027-01-30": {
+      "nightlyRate": 40
+    },
+    "2027-01-31": {
+      "nightlyRate": 40
+    },
+    "2027-02-01": {
+      "nightlyRate": 40
+    },
+    "2027-02-02": {
+      "nightlyRate": 40
+    },
+    "2027-02-03": {
+      "nightlyRate": 40
+    },
+    "2027-02-04": {
+      "nightlyRate": 40
+    },
+    "2027-02-05": {
+      "nightlyRate": 40
+    },
+    "2027-02-06": {
+      "nightlyRate": 40
+    },
+    "2027-02-07": {
+      "nightlyRate": 40
+    },
+    "2027-02-08": {
+      "nightlyRate": 40
+    },
+    "2027-02-09": {
+      "nightlyRate": 40
+    },
+    "2027-02-10": {
+      "nightlyRate": 40
+    },
+    "2027-02-11": {
+      "nightlyRate": 40
+    },
+    "2027-02-12": {
+      "nightlyRate": 40
+    },
+    "2027-02-13": {
+      "nightlyRate": 40
+    },
+    "2027-02-14": {
+      "nightlyRate": 40
+    },
+    "2027-02-15": {
+      "nightlyRate": 40
+    },
+    "2027-02-16": {
+      "nightlyRate": 40
+    },
+    "2027-02-17": {
+      "nightlyRate": 40
+    },
+    "2027-02-18": {
+      "nightlyRate": 40
+    },
+    "2027-02-19": {
+      "nightlyRate": 40
+    },
+    "2027-02-20": {
+      "nightlyRate": 40
+    },
+    "2027-02-21": {
+      "nightlyRate": 40
+    },
+    "2027-02-22": {
+      "nightlyRate": 40
+    },
+    "2027-02-23": {
+      "nightlyRate": 40
+    },
+    "2027-02-24": {
+      "nightlyRate": 40
+    },
+    "2027-02-25": {
+      "nightlyRate": 40
+    },
+    "2027-02-26": {
+      "nightlyRate": 40
+    },
+    "2027-02-27": {
+      "nightlyRate": 40
+    },
+    "2027-02-28": {
+      "nightlyRate": 40
+    },
+    "2027-03-01": {
+      "nightlyRate": 50
+    },
+    "2027-03-02": {
+      "nightlyRate": 50
+    },
+    "2027-03-03": {
+      "nightlyRate": 50
+    },
+    "2027-03-04": {
+      "nightlyRate": 50
+    },
+    "2027-03-05": {
+      "nightlyRate": 50
+    },
+    "2027-03-06": {
+      "nightlyRate": 50
+    },
+    "2027-03-07": {
+      "nightlyRate": 50
+    },
+    "2027-03-08": {
+      "nightlyRate": 50
+    },
+    "2027-03-09": {
+      "nightlyRate": 50
+    },
+    "2027-03-10": {
+      "nightlyRate": 50
+    },
+    "2027-03-11": {
+      "nightlyRate": 50
+    },
+    "2027-03-12": {
+      "nightlyRate": 50
+    },
+    "2027-03-13": {
+      "nightlyRate": 50
+    },
+    "2027-03-14": {
+      "nightlyRate": 50
+    },
+    "2027-03-15": {
+      "nightlyRate": 50
+    },
+    "2027-03-16": {
+      "nightlyRate": 50
+    },
+    "2027-03-17": {
+      "nightlyRate": 50
+    },
+    "2027-03-18": {
+      "nightlyRate": 50
+    },
+    "2027-03-19": {
+      "nightlyRate": 50
+    },
+    "2027-03-20": {
+      "nightlyRate": 50
+    },
+    "2027-03-21": {
+      "nightlyRate": 50
+    },
+    "2027-03-22": {
+      "nightlyRate": 50
+    },
+    "2027-03-23": {
+      "nightlyRate": 50
+    },
+    "2027-03-24": {
+      "nightlyRate": 50
+    },
+    "2027-03-25": {
+      "nightlyRate": 50
+    },
+    "2027-03-26": {
+      "nightlyRate": 50
+    },
+    "2027-03-27": {
+      "nightlyRate": 50
+    },
+    "2027-03-28": {
+      "nightlyRate": 50
+    },
+    "2027-03-29": {
+      "nightlyRate": 50
+    },
+    "2027-03-30": {
+      "nightlyRate": 50
+    },
+    "2027-03-31": {
+      "nightlyRate": 50
+    }
+  }
+};
